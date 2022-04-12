2f598abd224cce9739c928ea2149c587df5dcbcf
rajatkhatri7 <rajat@sentient.io>
2022-04-13 01:55:33 +0530

    database connected and created auth urls

--- connectingdots/authentication/urls.py
+++ connectingdots/authentication/urls.py
@@ -4,10 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('profile/', views.profile, name='profile'),
-    path('profile/files/', views.files, name='files')
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
 
 ]
--- connectingdots/authentication/views.py
+++ connectingdots/authentication/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
 
 # Create your views here.
 
@@ -6,17 +9,41 @@ def home(request):
     return render(request, 'authentication/home.html')
 
 def signup(request):
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.warning(request, 'Username exists')
+                return redirect('signup')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.warning(request,'email already exists')
+                    return redirect('signup')
+                else:
+                    user = User.objects.create_user(firstname=firstname, lastname=lastname,username=username, email=email, password=password)
+                    user.save()
+                    messages.success(request, 'Account created successfully')
+                    return redirect('login')
+        else:
+            messages.warning(request, 'Password do not match') 
+            return redirect('signup')
+
     return render(request, 'authentication/signup.html')
 
 
 def login(request):
     return render(request, 'authentication/login.html')
 
-def logout(request):
-    return render(request, 'authentication/logout.html')
 
-def profile(request):
-    return render(request, 'authentication/profile.html')
+def logout_user(request):
+    logout(request)
+    return redirect('home')
 
-def files(request):
-    return render(request, 'authentication/files.html')
+def dashboard(request):
+    return render(request, 'authentication/dashboard.html')
